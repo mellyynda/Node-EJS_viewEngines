6f08aa4568e930014259192a8a499ee24253dba6
Melinda <melinda.sava.td@gmail.com>
2021-05-10 15:00:19 +0200

    connected to MongoDB on Atlas, created model for blog and saved new blogs on db

--- app.js
+++ app.js
@@ -1,10 +1,60 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 //express app
 const app = express();
 
-//listen for requests
-app.listen(5000);
+//connect to mongoDB Atlas and start listening for requests after connection established
+const dbURI = 'mongodb+srv://mellyynda:user123456@nodetuts.82blk.mongodb.net/note-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(5000))
+    .catch(err => console.log(err));
+
+// middleware and static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+//mongose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog2',
+        snippet: 'about my new blog',
+        body: 'more about my new blog'
+    });
+    blog.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch(err => {
+            console.log(err);
+        });
+})
+
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+        .then(result => {
+            res.send(result)
+        })
+        .catch(err => console.log(err));
+})
+
+app.get('/single-blog', (req, res) => {
+    Blog.findById('60992b29917d5e3e98ebbf29')
+        .then(result => {
+            res.send(result)
+        })
+        .catch(err => console.log(err))
+})
+//middleware example
+// app.use((req, res, next) => {
+//     console.log("new request made");
+//     console.log("path: ", req.path);
+//     console.log("host: ", req.hostname);
+//     console.log("method", req.method);
+//     next();
+// });
 
 //register view engine
 app.set('view engine', 'ejs');
@@ -19,6 +69,10 @@ app.get('/', (req, res) => {
     ];
     res.render('index', { title: 'Home', blogs });
 });
+app.use((req, res, next) => {
+    console.log("in the next middleware");
+    next();
+})
 
 app.get('/about', (req, res) => {
     //res.send("<p>about page</p>");
