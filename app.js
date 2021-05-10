84218e5783a8602dfebdc01d71e24e4db0651eec
Melinda <melinda.sava.td@gmail.com>
2021-05-10 11:00:28 +0200

    fixed problem by changing to port 5000, local port 3000 was used for PostgreSQL, also cleand code a little bit

--- app.js
+++ app.js
@@ -3,20 +3,19 @@ const express = require('express');
 //express app
 const app = express();
 
+//listen for requests
+app.listen(5000);
+
 //register view engine
 app.set('view engine', 'ejs');
-
-//listen for requests
-
-app.listen(3000);
 
 app.get('/', (req, res) => {
     //res.send("<p>home page</p>");
     //res.sendFile('./views/index.html', { root: __dirname });
     const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' },
     ];
     res.render('index', { title: 'Home', blogs });
 });
@@ -28,8 +27,8 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/blogs/create', (req, res) => {
-   res.render('create', { title: 'Create a new blog' }); 
-})
+    res.render('create', { title: 'Create a new blog' });
+});
 
 //redirect
 app.get('/about-us', (req, res) => {
@@ -41,4 +40,4 @@ app.get('/about-us', (req, res) => {
 app.use((req, res) => {
     //res.status(404).sendFile('./views/404.html', { root: __dirname });
     res.status(404).render('404', { title: '404' });
-})
+});
